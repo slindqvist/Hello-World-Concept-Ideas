f580a71daf4c612e058e76700709f29430028c81
slindqvist33 <saralindqvist2@hotmail.com>
2020-09-10 14:57:07 +0200

    Diverse ting

---
+++ ConceptIdeasProject/Assets/01_Scripts/HowToSetUpDynamicWaterTutorial/Floater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    public Rigidbody _rigidBody;
+    public float _depthBeforeSubmerged = 1f;
+    public float _displacementAmount = 3f;
+    public int _floaterCount = 1;
+    public float _waterDrag = 0.99f;
+    public float _waterAngularDrag = 0.5f;
+
+    private void FixedUpdate() {
+        _rigidBody.AddForceAtPosition(Physics.gravity / _floaterCount, transform.position, ForceMode.Acceleration);
+        float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
+
+        if(transform.position.y < waveHeight) {
+            float displacementMultiplier = Mathf.Clamp01((waveHeight -transform.position.y) / _depthBeforeSubmerged) * _displacementAmount;
+            _rigidBody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            _rigidBody.AddForce(displacementMultiplier * -_rigidBody.velocity * _waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            _rigidBody.AddTorque(displacementMultiplier * -_rigidBody.angularVelocity * _waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ ConceptIdeasProject/Assets/01_Scripts/HowToSetUpDynamicWaterTutorial/WaveManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager instance;
+
+    public float _amplitude = 1f;
+    public float _length = 2f;
+    public float _speed = 1f;
+    public float _offset = 0f;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Debug.Log("Instance already exist, destroying this object!");
+            Destroy(this);
+        }
+    }
+
+    private void Update() {
+        _offset += Time.deltaTime * _speed;
+    }
+
+    public float GetWaveHeight(float _x) {
+        return _amplitude * Mathf.Sin(_x / _length + _offset);
+    }
+}
