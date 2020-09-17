0d36b956eb760069dcbdaa027799827aba0237f6
KevinOsth <kevin.osth@hotmail.com>
2020-09-17 09:14:11 +0200

    Lights
    Lights and lights skripts

---
+++ ConceptIdeasProject/Assets/01_Scripts/BookshelfSkripts/CubeOnRightPlace.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CubeOnRightPlace : MonoBehaviour
+{
+    public UnityEvent _cubeOnRightPlace;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        
+        
+        if (_cubeOnRightPlace != null)
+        {
+            _cubeOnRightPlace.Invoke();
+        }
+    }
+}
---
+++ ConceptIdeasProject/Assets/01_Scripts/BookshelfSkripts/LigthRandom.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LigthRandom : MonoBehaviour
+{
+      public GameObject[] _lights;
+      private int _lightNumber; 
+      //public List<GameObject> _lightsList = new List<GameObject>();
+
+    public void Start()
+    {
+        _lightNumber = Random.Range(0, _lights.Length);
+        for (int i = 0; i < _lights.Length; i++)
+            //_lights[i].SetActive(false);
+            _lights[_lightNumber].SetActive(true);
+
+        /* for (int i = 0; i < _lightsList.Count; i++) 
+         {
+             int j = Random.Range(i, _lightsList.Count);
+             GameObject t = _lightsList[i];
+             _lightsList[i] = _lightsList[j];
+             _lightsList[j] = t;
+             _lightsList[i].SetActive(false);
+         } */
+    }
+
+    public void Update()
+    {
+       
+
+            
+
+           /* for (int k = 0; k < _lightsList.Count; k++)
+            {
+                _lightsList[k].SetActive(false);
+
+                
+
+                
+            }*/
+         
+    }
+
+    public void RandomLights()
+    {
+        _lightNumber = Random.Range(0, _lights.Length);
+
+        for (int i = 0; i < _lights.Length; i++)
+        {
+            _lights[i].SetActive(false);
+
+
+            _lights[_lightNumber].SetActive(true);
+        }
+
+    }
+
+
+}
