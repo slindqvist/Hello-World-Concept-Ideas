d8bed3c8f50dce9fdf9d61888cb3f966a686873e
slindqvist33 <saralindqvist2@hotmail.com>
2020-10-16 09:52:29 +0200

    - Adding spawners

--- ConceptIdeasProject/Assets/01_Scripts/RodTrigger.cs
+++ ConceptIdeasProject/Assets/01_Scripts/RodTrigger.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RodTrigger : MonoBehaviour
-{
+public class RodTrigger : MonoBehaviour {
     public Transform _stand;
-
-    private Renderer _rodRenderer;
-    private Rigidbody _rodRigidbody;
-    private bool _hasSpawned;
-
     public float _respawnTime = 2f;
 
+    private Material[] _previousMaterial;
+    private Renderer[] _rodRenderers;
+    private bool _hasSpawned;
+    
     private void Start() {
-        _rodRenderer = GetComponent<Renderer>();
-        _rodRigidbody = GetComponent<Rigidbody>();
+        _rodRenderers = GetComponentsInChildren<Renderer>();
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -24,23 +21,30 @@ public class RodTrigger : MonoBehaviour
 
         if (other.gameObject.CompareTag("Shark")) {
             DestroyRod();
+            Debug.Log("Shark found");
         }
     }
 
     private void DestroyRod() {
-        _rodRenderer.enabled = false;
+        foreach (Renderer renderer in _rodRenderers) {
+            renderer.enabled = false;
+        }
+
         Invoke("RespawnRod", _respawnTime);
-        _rodRigidbody.isKinematic = true;
         _hasSpawned = false;
+        Debug.Log("Rod destroyed");
     }
 
     private void RespawnRod() {
-        if(_hasSpawned == false) {
+        if (_hasSpawned == false) {
             transform.position = _stand.position;
             transform.rotation = _stand.rotation;
-            _rodRenderer.enabled = true;
-            _rodRigidbody.isKinematic = false;
+
+            foreach (Renderer renderer in _rodRenderers) {
+                renderer.enabled = true;
+            }
             _hasSpawned = true;
+            Debug.Log("Rod respawned");
         }
     }
 }
---
+++ ConceptIdeasProject/Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI _timerText;
+    public TextMeshProUGUI _gameOverText;
+    public float _totalTime;
+
+    private float _minutes;
+    private float _seconds;
+
+    void Start()
+    {
+        _gameOverText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
