8e8ad79f1b4234be9006a5b4a7e4488511f2bf6c
slindqvist33 <saralindqvist2@hotmail.com>
2020-10-28 09:16:59 +0100

    - Small changes

--- ConceptIdeasProject/Assets/01_Scripts/SceneHandler.cs
+++ ConceptIdeasProject/Assets/01_Scripts/SceneHandler.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
+using Valve.VR;
 using Valve.VR.Extras;
 
-public class SceneHandler : MonoBehaviour
-{
-    public SteamVR_LaserPointer _laserPointer;
+public class SceneHandler : MonoBehaviour {
+    public SteamVR_Input_Sources _hand;
 
+    public SteamVR_LaserPointer _laserPointer;
     public GameObject _rensaVattnetPlaceHolder;
 
     private void Awake() {
         _laserPointer.PointerIn += PointerInside;
         _laserPointer.PointerOut += PointerOutside;
         _laserPointer.PointerClick += PointerClick;
+
+        _laserPointer.active = false;
     }
 
     public void PointerClick(object sender, PointerEventArgs e) {
@@ -42,6 +45,13 @@ public class SceneHandler : MonoBehaviour
         }
         else if (e.target.name == "Bokhyllan") {
             Debug.Log("Bokhyllan was exited");
+        }
+    }
+
+    protected void Update() {
+        if (SteamVR_Actions.default_GrabGrip.GetLastStateDown(_hand)){
+            _laserPointer.active = true;
+            Debug.Log("Grip was touched");
         }
     }
 }
